1801c47a726d2bc935382d9ac16564a2537f1875
Jeffrey B. Daube <daubejb@gmail.com>
2017-08-24 12:28:47 -0400

    halfway through converting all we components to js string literal templates

---
+++ daube-header.js
@@ -0,0 +1,46 @@
+(function() {
+    let thisImportDoc = document.currentScript.ownerDocument;
+    class DaubeHeader extends HTMLElement {
+      static get template() {
+      return `<link rel="stylesheet" href="../shared.css">
+<style>
+  :host {
+    display: block;
+  }
+  header {
+    display: inline-block;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    background-color: var(--primary-color);
+    color: var(--text-primary-white);
+    height: 3.5rem;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+  }
+  .maintitle {
+    padding: 0.92rem 0rem 0.92rem 1.5rem;
+    font-weight: 200;
+    font-size: 1.66rem;
+    height: 3.5rem;
+  }
+</style>
+<header>
+  <div class="maintitle">Header</div>
+  <slot></slot>
+</header>`
+      }
+      static get observedAttributes() {}
+      constructor() {
+        super();
+        var shadowRoot = this.initShadowDom();
+      }
+      connectedCallback() {}
+      initShadowDom() {
+        let shadowRoot = this.attachShadow({mode: 'open'});
+        let tmpl = DaubeHeader.template;
+        shadowRoot.innerHTML = tmpl;
+        return shadowRoot;
+      }
+     } // Class DaubeInputText
+    customElements.define("daube-header", DaubeHeader)
+  })();
---
+++ daube-radio.js
@@ -0,0 +1,77 @@
+
+(function() {
+  let thisImportDoc = document.currentScript.ownerDocument;
+  class DaubeRadio extends HTMLElement {
+    static get observedAttributes() {}
+    constructor() {
+      super();
+      var shadowRoot = this.initShadowDom();
+    }
+    static get template() {
+      return `<link rel="stylesheet" href="../shared.css">
+<style>
+  :host { font-size: 1rem; }
+  /* State - idle + empty **************************/
+  #radiogroup {
+    margin: 2rem 0;
+    display: block;
+    position: relative;
+    max-width: 45rem;
+  }
+  #radios {
+    padding-top: 1.5rem;
+    display: block;
+  }
+  #radiogrouptitle {
+    position: absolute;
+    font-size: 0.95rem;
+    top: 0rem;
+    background-color: rgba(0,0,0,0);
+    color: rgba(0,0,0,0.54);
+    pointer-events: none;
+  }
+  input {
+    background-color: rgba(0,0,0,0);
+    display: inline-block;
+    padding: 0.5rem 0.5rem 0.5rem 0.5rem;
+    margin-bottom: 0.5rem;
+    margin-right: 0.4rem;
+    border: none;
+    line-height: 1.15;
+    border-bottom: 1px solid rgba(0,0,0,0.42);
+    height: 1.5rem;
+    width: 1.5rem;
+  }
+  label {
+    font-size: 0.95rem;
+    color: var(--pb-normal-text);
+    opacity: 0.87;
+    margin-bottom: 1rem;
+    vertical-align: middle;
+    display: inline-block;
+  }
+  input:hover, {
+    border: 2px solid var(--primary-color);
+  }
+  input:checked+label {
+    color: var(--primary-color)
+  }
+</style>
+<div id="radiogroup">
+  <div id="radios">
+    <input type="radio" id="radio1" name="radiogroup" value="option 1"><label id="radiovalue1">Option 1</label><br>
+    <input type="radio" id="radio2" name="radiogroup" value="option 2"><label id="radiovalue2">Option 2</label><br>
+  </div>
+  <div id="radiogrouptitle">Radio Group</div>
+</div>`
+    }
+    connectedCallback() {}
+    initShadowDom() {
+      let shadowRoot = this.attachShadow({mode: 'open'});
+      let tmpl = DaubeRadio.template;
+      shadowRoot.innerHTML = tmpl;
+      return shadowRoot;
+    }
+  } // Class DaubeRadio
+  customElements.define('daube-radio', DaubeRadio)
+})();
